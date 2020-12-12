c2305aafb0680c7b504719351dd96f2ba57740d4
Peter Slavin <pbslavin@gmail.com>
2020-12-12 17:26:31 -0600

    implemented navbar Events dropdown

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,8 +1,12 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '../user/auth.service';
+import { Event } from '../events/shared/event.model';
 import { EventService } from '../events/shared/event.service';
 import { User } from '../user/user.model';
 import { Session } from '../events/shared/event.model';
+import { Observable } from 'rxjs';
+import { EventListResolver } from '../events';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'nav-bar',
@@ -14,11 +18,18 @@ import { Session } from '../events/shared/event.model';
         li > a.active { color: #F98935; }
     `]
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
     searchTerm: string = "";
     foundSessions: Session[];
+    events: Event[];
 
-    constructor(private auth: AuthService, private eventService: EventService) {
+    constructor(private auth: AuthService, private eventService: EventService, private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        this.eventService.getEvents().subscribe(events => {
+            this.events = events;
+        });
     }
 
     searchSessions(searchTerm: string) {
