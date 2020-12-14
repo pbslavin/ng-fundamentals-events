a47fef61198f387d709706644a127ab67ce6ee91
Peter Slavin <pbslavin@gmail.com>
2020-12-14 09:54:07 -0600

    reinstated accidentally deleted addVoter method

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -1,7 +1,6 @@
 import { VoterService } from './voter.service';
 import { Session } from '../shared/event.model';
 import { Observable, of } from 'rxjs';
-import { markParentViewsForCheckProjectedViews } from '@angular/core/src/view/util';
 
 describe('VoterService', () => {
     let voterService: VoterService,
@@ -17,14 +16,18 @@ describe('VoterService', () => {
         it('should remove the voter from the list of voters', () => {
             var session = { id: 6, voters: ['joe', 'john'] };
             mockHttp.delete.and.returnValue(of(false));
-            console.log(session.voters);
-
             voterService.deleteVoter(3, <Session>session, 'joe');
-            
-            console.log(session.voters);
 
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe('john');
-        })
-    })
-})
+        });
+
+        it('should call http.delete with the right URL', () => {
+            var session = { id: 6, voters: ['joe', 'john'] };
+            mockHttp.delete.and.returnValue(of(false));
+            voterService.deleteVoter(3, <Session>session, 'joe');
+
+            expect()
+        });
+    });
+});
--- src/app/events/event-details/voter.service.ts
+++ src/app/events/event-details/voter.service.ts
@@ -9,22 +9,24 @@ export class VoterService {
     constructor(private http: HttpClient) {}
 
     deleteVoter(eventId: number, session: Session, voterName: string) {
-        session.voters = session.voters.filter(v => v !== voterName);
+      session.voters.splice(session.voters.indexOf(voterName), 1);
 
-        const url = `api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
-         this.http.delete(url)
-            .pipe(catchError(this.handleError('deleteVoter')))
-            .subscribe(); 
-    }
-    addVoter(eventId: number, session: Session, voterName: string) {
-        session.voters.push(voterName);
+      const url = `api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+      this.http.delete(url)
+        .pipe(catchError(this.handleError('deleteVoter')))
+        .subscribe(); 
+  }
 
-        const options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
-        const url = `api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
-        return this.http.post(url, {}, options)
-            .pipe(catchError(this.handleError('addVoter')))
-            .subscribe();
-    }
+  addVoter(eventId: number, session: Session, voterName: string) {
+      session.voters.push(voterName);
+
+      const options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+      const url = `api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+      return this.http.post(url, {}, options)
+          .pipe(catchError(this.handleError('addVoter')))
+          .subscribe();
+  }
+
     userHasVoted(eventId: number, session: Session, voterName: string) {
         return session.voters.some(voter => voter === voterName);
     }
