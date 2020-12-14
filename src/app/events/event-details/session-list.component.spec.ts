33e1556fa620e6c3ec6183c7b85ad44884e1c825
Peter Slavin <pbslavin@gmail.com>
2020-12-14 15:44:31 -0600

    component test being skipped for unknown reason

---
+++ src/app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,34 @@
+import { SessionListComponent } from './session-list.component';
+import { Session } from '../shared/event.model';
+
+describe('SessionListComponent', () => {
+    let component: SessionListComponent;
+    let mockAuthService, mockVoterService;
+
+    beforeEach(() => {
+        component = new SessionListComponent(mockAuthService, mockVoterService);
+    });
+
+    describe('ngOnChanges', () => {
+        it('should filter the sessions correctly'), () => {
+            component.sessions = <Session[]>[{ name: 'session 1', level: 'intermediate' }, { name: 'session 2', level: 'intermediate' }, { name: 'session 3', level: 'beginner' }];
+            component.filterBy = 'intermediate';
+            component.sortBy = 'name';
+            component.eventId = 3;
+
+            component.ngOnChanges()
+            expect(component.visibleSessions.length).toEqual(2);
+            // expect(JSON.stringify(component.visibleSessions)).toEqual(JSON.stringify([{name: 'session 1', level: 'intermediate' }, { name: 'session 2', level: 'intermediate' }]));
+        };
+
+        it('should sort the sessions correctly'), () => {
+            component.sessions = <Session[]>[{ name: 'session 2', level: 'intermediate' }, { name: 'session 3', level: 'intermediate' }, { name: 'session 1', level: 'beginner' }];
+            component.filterBy = 'all';
+            component.sortBy = 'name';
+            component.eventId = 3;
+
+            component.ngOnChanges()
+            expect(component.visibleSessions[0].name).toBe('session 1');
+        };
+    });
+});
